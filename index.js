a6cf1543d0356fe59cd1bedd07c9af74119ef5a4
mrkm86 <34387092+mrkm86@users.noreply.github.com>
2018-12-07 21:10:49 +0900

    Improve

---
+++ index.js
@@ -0,0 +1,47 @@
+//おまじない
+"use strict";
+
+//module.exports->ほかのモジュールから読み込み可能
+
+let oracle = require("./service/oracle");
+let googlehomenotifier = require("./service/google-home-notifier");
+
+console.log("Application is running...");
+
+///Notification取得
+return oracle.get_notification().then(
+    (response) => {
+        
+        //該当なし
+        if (response.length == 0) {
+
+            console.log("There is no notifications.");
+            console.log("Application is exit.");
+            return;
+
+        }
+        //該当あり
+        else {
+
+            console.log("----------------------------------------------------------------------");
+            console.log("Notification Got.");
+            console.log(response[0].t_message);
+            console.log("----------------------------------------------------------------------");
+
+            //GOOGLE-HOMEに通知を送る
+            var strmessage = response[0].t_message;
+            console.log(strmessage);
+            googlehomenotifier.NotifyGoogleHome(strmessage);
+
+            //通知を送ったので、API経由で通知を削除する
+            return oracle.put_remove_notification().then(
+                (response) => {
+
+                    //成功
+                    console.log("Notification is removed.");
+                    console.log("Application is exit.");
+                }
+            );
+        }
+    }
+);
