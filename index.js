ffd31c793bcf757eab27d588878527f51a9bb351
mrkm86 <34387092+mrkm86@users.noreply.github.com>
2018-12-17 22:09:46 +0900

    update for multiple messages

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-//おまじない
 "use strict";
-
-//module.exports->ほかのモジュールから読み込み可能
 
 let oracle = require("./service/oracle");
 let googlehomenotifier = require("./service/google-home-notifier");
 
 console.log("Application is running...");
 
-///Notification取得
+//get notification(s)
 return oracle.get_notification().then(
     (response) => {
 
@@ -24,32 +21,31 @@ return oracle.get_notification().then(
         else {
 
             console.log("----------------------------------------------------------------------");
-            console.log("Notification Got.");
-            console.log(response[0].t_message);
-            console.log(response[0].t_google_home_ip_address);
-            console.log(response[0].t_row_id);
+            console.log(`${response.length} Notification(s) Got.`);
             console.log("----------------------------------------------------------------------");
 
-            //GOOGLE-HOMEに通知を送る
-            var strMessage = response[0].t_message;
-            var strIPAddress = response[0].t_google_home_ip_address;
-            var strRowId = response[0].t_row_id;
+            response.forEach( function( value ) {
 
-            //for debug
-            strRowId = "AAAWixAAAAACALjAAA";
+                //notify GoogleHome
+                var strMessage = value.t_message;
+                var strIPAddress = value.t_google_home_ip_address;
+                var strRowId = value.t_row_id;
 
-            return googlehomenotifier.NotifyGoogleHome(strMessage, strIPAddress)
-                .then((response) => {
-                    console.log(response);
-                }).then(() => {
-                    oracle.delete_notification(strRowId).then(
-                        (response) => {
-                            //成功
-                            console.log("Notification is removed.");
-                            console.log("Application is exit.");
-                        }
-                    );
-                });
+                console.log(`Notification(${strRowId}) is started.`);
+
+                return googlehomenotifier.NotifyGoogleHome(strMessage, strIPAddress)
+                    .then((response) => {
+                        console.log(`Notification(${strRowId}) is notified.`);
+                    }).then(() => {
+                        oracle.delete_notification(strRowId).then(
+                            (response) => {
+                                console.log(`Notification(${strRowId}) is removed.`);
+                            }
+                        ).then((response) => {
+                            console.log(`Notification(${strRowId}) is completed.`);
+                        });
+                    });
+            })
         }
     }
 );
--- service/google-home-notifier.js
+++ service/google-home-notifier.js
@@ -5,28 +5,27 @@ var googlehome = require('../node_modules/google-home-notifier');
 
 var GOOGLE_HOME_NOTIFIER_LANGUAGE = config["GOOGLE_HOME_NOTIFIER_LANGUAGE"];
 var GOOGLE_HOME_NOTIFIER_DEVICE_NAME = config["GOOGLE_HOME_NOTIFIER_DEVICE_NAME"];
-var GOOGLE_HOME_NOTIFIER_IPADDRESS = config["GOOGLE_HOME_NOTIFIER_IPADDRESS"];
 
 console.log("----------------------------------------------------------------------");
 console.log("GOOGLE_HOME_NOTIFIER_LANGUAGE is ..." + GOOGLE_HOME_NOTIFIER_LANGUAGE);
 console.log("GOOGLE_HOME_NOTIFIER_DEVICE_NAME is ..." + GOOGLE_HOME_NOTIFIER_DEVICE_NAME);
-console.log("GOOGLE_HOME_NOTIFIER_IPADDRESS is ..." + GOOGLE_HOME_NOTIFIER_IPADDRESS);
 console.log("----------------------------------------------------------------------");
-
-//値を格納
-googlehome.device(GOOGLE_HOME_NOTIFIER_DEVICE_NAME, GOOGLE_HOME_NOTIFIER_LANGUAGE);
-googlehome.ip(GOOGLE_HOME_NOTIFIER_IPADDRESS, GOOGLE_HOME_NOTIFIER_LANGUAGE);
 
 module.exports = class ServiceGoogleHomeNotification {
 
     //通知をする
     static async NotifyGoogleHome(strMessage, strIPAddress) {
 
-        //IPアドレスを格納
+        console.log(`set googlehome.device(${GOOGLE_HOME_NOTIFIER_DEVICE_NAME},${GOOGLE_HOME_NOTIFIER_LANGUAGE})`);
+        console.log(`set googlehome.ip(${strIPAddress},${GOOGLE_HOME_NOTIFIER_LANGUAGE})`);
+
+        //set value
+        googlehome.device(GOOGLE_HOME_NOTIFIER_DEVICE_NAME, GOOGLE_HOME_NOTIFIER_LANGUAGE);
         googlehome.ip(strIPAddress, GOOGLE_HOME_NOTIFIER_LANGUAGE);
 
         return new Promise((resolve, reject) => {
             try {
+                console.log(`googlehome.notify(${strMessage})`);
                 googlehome.notify(strMessage, function (notifyRes) {
 
                     try {
